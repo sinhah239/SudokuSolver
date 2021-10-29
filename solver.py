ef4167efa5d19512e255e0327f700bfdaf7d40de
Himanshu <sinhah239@gmail.com>
2021-10-29 15:15:17 -0400

    Implemented solution using backtracking algorithm

--- solver.py
+++ solver.py
@@ -1,10 +1,20 @@
 """
 NAME 
     solver
+
 DESCRIPTION
-    A simple sudoku solver written with the backtracking algorithm
-    all credit goes to TechWithTime @ https://www.techwithtim.net/
-    this was written for personal, educational purposes
+    a simple sudoku solver written with the backtracking algorithm
+    all credit goes to techwithtim @ https://www.techwithtim.net/
+    written for personal, educational purposes
+
+FUNCTIONS 
+    solve(bo)
+
+    valid(bo, num, pos)
+
+    print_board(bo)
+
+    find_empty(bo)
 """
 
 board = [
@@ -17,23 +27,94 @@ board = [
     [0,7,0,3,0,0,0,1,2],
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
-]
+] # the starting board that needs to be solved
 
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+    
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+    
 def print_board(bo):
+    """
+    DESCRIPTION
+    prints a 9x9 sudoku board  
+    visually seperates each 3x3 box 
 
+    PARAMETERS 
+    bo: 9x9 board to be printed 
+    """
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("---------------------")
+            print("---------------------")  
 
-        for j in range(len(bo[i])):
+        for j in range(len(bo[i])): 
             if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+                print(" | ", end="") 
 
             if j == 8:
-                print(bo[i][j])
-            elif j == 2 or j == 5:
+                print(bo[i][j]) 
+            elif j == 2 or j == 5: 
                 print(bo[i][j], end="")
-            else:
-                print(str(bo[i][j]) + " ", end="")
+            else: 
+                print(str(bo[i][j]) + " ", end="") 
 
+def find_empty(bo):
+    """
+    DESCRIPTION
+    finds the first empty spot in a 9x9 sudoku board 
+    empty spots contain 0
+    searches top to bottom, left to right
+
+    PARAMETERS
+    bo: 9x9 board to be searched 
+
+    RETURN 
+    returns a tuple containing the location of the empty spot
+    """
+    for i in range(len(bo)):
+        for j in range(len(bo[i])):
+            if bo[i][j] == 0:
+                return (i, j) # row, col 
+
+    return None
+
+
+print_board(board)
+solve(board)
+print("                             ")
 print_board(board)
